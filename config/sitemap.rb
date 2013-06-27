9c946411c3f0b1cc0591c2aa60b3d1c5c6337ef6
hhanckes <hhanckes@gmail.com>
2013-06-27 17:49:06 -0400

    Arreglo a la generación del sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,7 +6,7 @@ SitemapGenerator::Sitemap.create do
   add '/chile', :changefreq => 'daily', :priority => 1
     
   Dress.all.each do |dress|
-    add dress_ver_path(:type => dress.type, :slug => dress.slug, :country_url_path => 'chile'), :lastmod => dress.updated_at, :priority => 0.9
+    add dress_ver_path(:type => dress.dress_type.name, :slug => dress.slug, :country_url_path => 'chile'), :lastmod => dress.updated_at, :priority => 0.9
   end
   
   Post.find_each do |post|
