3cbf21b9812546ad1f44982b053f7ad42e3435ed
hhanckes <hhanckes@gmail.com>
2013-07-05 20:17:01 -0400

    Arreglo para ver carrito de compra

--- app/models/size.rb
+++ app/models/size.rb
@@ -2,6 +2,6 @@ class Size < ActiveRecord::Base
 	has_many :dress_stock_sizes, :dependent => :destroy
 	has_many :dresses, :through => :dress_stock_sizes
 	has_and_belongs_to_many :dress_types
-  
+  validates_uniqueness_of :name
 	
 end
