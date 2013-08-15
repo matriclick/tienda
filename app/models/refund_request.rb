59af747d5bc48dea5759c6b86da4144e39bf11e9
hhanckes <hhanckes@gmail.com>
2013-08-15 13:42:51 -0400

    Cambios al request form

--- app/models/refund_request.rb
+++ app/models/refund_request.rb
@@ -3,5 +3,5 @@ class RefundRequest < ActiveRecord::Base
   belongs_to :user
 	belongs_to :dress
 	
-  validates :refund_reason_id, :user_id, :account_owner_name, :account_owner_id, :account_owner_email, :account_bank, :account_type, :account_number, :presence => true
+  validates :refund_reason_id, :user_id, :account_owner_name, :account_owner_id, :account_owner_email, :account_bank, :account_type, :account_number, :dress_id, :presence => true
 end
