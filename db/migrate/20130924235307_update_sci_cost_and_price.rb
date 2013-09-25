0397b21f727ece85673431e7aa8b216b4432defd
Hans <hhanckes@gmail.com>
2013-09-24 22:14:51 -0400

    nuevo intento

--- db/migrate/20130924235307_update_sci_cost_and_price.rb
+++ db/migrate/20130924235307_update_sci_cost_and_price.rb
@@ -1,8 +1,8 @@
 class UpdateSciCostAndPrice < ActiveRecord::Migration
   def up
     ShoppingCartItem.all.each do |sci|
-      puts sci.purchasable_id
-      if !Dress.find(sci.purchasable_id).blank?
+      if Dress.exists?(sci.purchasable_id)
+        puts sci.purchasable.introduction
         vat = sci.purchasable.vat_cost.nil? ? 0 : sci.purchasable.vat_cost
         net = sci.purchasable.net_cost.nil? ? 0 : sci.purchasable.net_cost
         amount = sci.quantity.nil? ? 0 : sci.quantity
@@ -12,7 +12,7 @@ class UpdateSciCostAndPrice < ActiveRecord::Migration
         sci.price = sci.purchasable.price
         sci.save
       else
-        sci.destroy 
+        sci.destroy
       end
     end
   end
