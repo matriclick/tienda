987b073ce51d77e15d517d59f7a7f9c00dece103
Hans <hhanckes@gmail.com>
2013-09-24 20:35:37 -0400

    nuevo arreglo a la migración

--- db/migrate/20130924235307_update_sci_cost_and_price.rb
+++ db/migrate/20130924235307_update_sci_cost_and_price.rb
@@ -1,7 +1,7 @@
 class UpdateSciCostAndPrice < ActiveRecord::Migration
   def up
     ShoppingCartItem.all.each do |sci|
-      if !sci.purchasable.nil?
+      if !Dress.where(id: sci.purchasable_id).nil?
         vat = sci.purchasable.vat_cost.nil? ? 0 : sci.purchasable.vat_cost
         net = sci.purchasable.net_cost.nil? ? 0 : sci.purchasable.net_cost
         amount = sci.quantity.nil? ? 0 : sci.quantity
