1a0b97a4b27f33eb52a8b9a99e59c5c3d21dd341
Hans <hhanckes@gmail.com>
2013-09-24 20:27:42 -0400

    Arreglo al migrate de sci's

--- db/migrate/20130924235307_update_sci_cost_and_price.rb
+++ db/migrate/20130924235307_update_sci_cost_and_price.rb
@@ -1,14 +1,18 @@
 class UpdateSciCostAndPrice < ActiveRecord::Migration
   def up
     ShoppingCartItem.all.each do |sci|
-      vat = sci.purchasable.vat_cost.nil? ? 0 : sci.purchasable.vat_cost
-      net = sci.purchasable.net_cost.nil? ? 0 : sci.purchasable.net_cost
-      amount = sci.quantity.nil? ? 0 : sci.quantity
+      if !sci.purchasable.nil?
+        vat = sci.purchasable.vat_cost.nil? ? 0 : sci.purchasable.vat_cost
+        net = sci.purchasable.net_cost.nil? ? 0 : sci.purchasable.net_cost
+        amount = sci.quantity.nil? ? 0 : sci.quantity
     
-      sci.total_cost = (vat+net)*amount
-      sci.unit_cost = (vat+net)
-      sci.price = sci.purchasable.price
-      sci.save
+        sci.total_cost = (vat+net)*amount
+        sci.unit_cost = (vat+net)
+        sci.price = sci.purchasable.price
+        sci.save
+      else
+        sci.destroy 
+      end
     end
   end
 
