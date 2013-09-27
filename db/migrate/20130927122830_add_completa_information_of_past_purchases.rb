f171e0adda72d5688964ba99bb06a3f30fc1224c
Hans <hhanckes@gmail.com>
2013-09-27 09:35:14 -0300

    mejora a la migración

--- db/migrate/20130927122830_add_completa_information_of_past_purchases.rb
+++ db/migrate/20130927122830_add_completa_information_of_past_purchases.rb
@@ -2,6 +2,7 @@ class AddCompletaInformationOfPastPurchases < ActiveRecord::Migration
   def up
     ShoppingCartItem.all.each do |sci|
       if !sci.quantity.blank?
+        puts sci.id
         sci.update_price if sci.price.blank?
         sci.update_costs if sci.unit_cost.blank? or sci.unit_cost == 0
         sci.store_paid = true
