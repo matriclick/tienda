521c360b91a1e0496ecec081c8091e874acfe6f4
Hans <hhanckes@gmail.com>
2013-09-26 22:45:09 -0300

    arreglo a migración

--- db/migrate/20130927005910_update_all_dresses_costs.rb
+++ db/migrate/20130927005910_update_all_dresses_costs.rb
@@ -3,7 +3,7 @@ class UpdateAllDressesCosts < ActiveRecord::Migration
     SupplierAccount.all.each do |sa|
       sa.dresses.each do |d|
         if d.net_cost.blank?
-          d.net_cost = d.price - d.price * sa.net_margin/100 * 1.19 if !d.price.blank? and !sa.net_margin.blank?
+          d.net_cost = d.price.to_f - d.price.to_f * sa.net_margin.to_f/100 * 1.19 if !d.price.blank? and !sa.net_margin.blank?
           puts '****'
           puts d.introduction
           puts d.net_cost
