73d37ad779ab7f8087ebf14ac9aea90aebe2f31d
Hans <hhanckes@gmail.com>
2013-09-27 09:44:30 -0300

    puts en migración

--- db/migrate/20130927122830_add_completa_information_of_past_purchases.rb
+++ db/migrate/20130927122830_add_completa_information_of_past_purchases.rb
@@ -4,9 +4,13 @@ class AddCompletaInformationOfPastPurchases < ActiveRecord::Migration
       if !sci.quantity.blank?
         puts sci.id
         sci.update_price if sci.price.blank?
+        puts sci.price 
         sci.update_costs if sci.unit_cost.blank? or sci.unit_cost == 0
+        puts sci.unit_cost
         sci.store_paid = true
+        puts sci.store_paid
         sci.save(:validate => false)
+        puts '********'
       end
     end
   end
