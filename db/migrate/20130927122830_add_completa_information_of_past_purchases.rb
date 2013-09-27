b2f0b8dd7394fc62332ea4ba2be5d1df6c201884
Hans <hhanckes@gmail.com>
2013-09-27 09:34:31 -0300

    Agregada migración para completar costos de shopping_cart_items

---
+++ db/migrate/20130927122830_add_completa_information_of_past_purchases.rb
@@ -0,0 +1,15 @@
+class AddCompletaInformationOfPastPurchases < ActiveRecord::Migration
+  def up
+    ShoppingCartItem.all.each do |sci|
+      if !sci.quantity.blank?
+        sci.update_price if sci.price.blank?
+        sci.update_costs if sci.unit_cost.blank? or sci.unit_cost == 0
+        sci.store_paid = true
+        sci.save
+      end
+    end
+  end
+
+  def down
+  end
+end
