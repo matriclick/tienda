5d7e1dbc538fe277d9b1258190f785a61af2a271
Hans <hhanckes@gmail.com>
2013-10-02 15:55:08 -0300

    arreglo a dress-type

--- app/models/dress_type.rb
+++ app/models/dress_type.rb
@@ -3,19 +3,7 @@ class DressType < ActiveRecord::Base
   has_and_belongs_to_many :sizes
   
   def self.get_options(supplier_account)
-    if supplier_account.nil?
-      return DressType.all.sort_by {|dt| dt[:name]}
-    else
-      sa_type = supplier_account.supplier_account_type.name
-      
-      if sa_type == "Ropa de Bebe"
-        return DressType.where('name like "%bebe%"').sort_by {|dt| dt[:name]}
-      elsif sa_type == "Tu Casa"
-        return DressType.where('name like "%tu-casa%"').sort_by {|dt| dt[:name]}
-      else
-        return DressType.where('name not like "%bebe%" and name not like "%tu-casa%"').sort_by {|dt| dt[:name]}
-      end
-    end
+    return DressType.order 'name'
   end
   
   def get_human_name
