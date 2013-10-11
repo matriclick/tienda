e2586a15fb3610c923c0d637cdf1b3cb8d9484e4
Hans <hhanckes@gmail.com>
2013-10-11 17:13:27 -0300

    migración OK

--- db/migrate/20131011195426_mark_all_dresses_from_tramanta_as_sold.rb
+++ db/migrate/20131011195426_mark_all_dresses_from_tramanta_as_sold.rb
@@ -1,5 +1,15 @@
 class MarkAllDressesFromTramantaAsSold < ActiveRecord::Migration
   def up
+    ShoppingCartItem.all.each do |sci|
+      unless sci.purchasable.nil?
+        unless sci.purchasable.supplier_account.nil?
+          if sci.purchasable.supplier_account.fantasy_name == 'Tramanta'
+            sci.store_paid = true
+            sci.save(validate: false)
+          end
+        end
+      end
+    end
   end
 
   def down
