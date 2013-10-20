c5d045a5676e75b2af8b5b7e208db7e58b55af4b
Hans <hhanckes@gmail.com>
2013-10-20 10:48:16 -0300

    migracion para dejar todas las de picaflor como no pagada

---
+++ db/migrate/20131020134131_update_paid_items_by_store.rb
@@ -0,0 +1,27 @@
+class UpdatePaidItemsByStore < ActiveRecord::Migration
+  def up
+    Purchase.all.each do |p|
+      puts p.created_at
+      if p.purchasable_type == 'Dress'
+        unless p.purchasable.blank?
+          if p.purchasable.supplier_account.fantasy_name == 'Picaflor Tienda'
+            p.store_paid = false
+            p.save
+          end
+        end
+      else
+        unless p.purchasable.blank?
+          p.purchasable.shopping_cart_items.each do |sci|
+            if sci.purchasable.supplier_account.fantasy_name == 'Picaflor Tienda'
+              sci.store_paid = false
+              sci.save
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def down
+  end
+end
