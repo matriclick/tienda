7875daf61182f13b5fe88667119208e5cc5c5f00
Hans <hhanckes@gmail.com>
2013-10-20 10:49:04 -0300

    arreglo a la migración

--- db/migrate/20131020134131_update_paid_items_by_store.rb
+++ db/migrate/20131020134131_update_paid_items_by_store.rb
@@ -4,7 +4,7 @@ class UpdatePaidItemsByStore < ActiveRecord::Migration
       puts p.created_at
       if p.purchasable_type == 'Dress'
         unless p.purchasable.blank?
-          if p.purchasable.supplier_account.fantasy_name == 'Picaflor Tienda'
+          if p.purchasable.supplier_account.fantasy_name == 'Pajarita Boutique'
             p.store_paid = false
             p.save
           end
@@ -12,7 +12,7 @@ class UpdatePaidItemsByStore < ActiveRecord::Migration
       else
         unless p.purchasable.blank?
           p.purchasable.shopping_cart_items.each do |sci|
-            if sci.purchasable.supplier_account.fantasy_name == 'Picaflor Tienda'
+            if sci.purchasable.supplier_account.fantasy_name == 'Pajarita Boutique'
               sci.store_paid = false
               sci.save
             end
