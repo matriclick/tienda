86bd52404a6115ffb26be54b7dc002a1dbafd1df
Hans <hhanckes@gmail.com>
2014-05-06 14:03:08 -0400

    migration para arreglar net_cost

--- db/migrate/20140506170625_recalculate_cost_to_dresses.rb
+++ db/migrate/20140506170625_recalculate_cost_to_dresses.rb
@@ -1,5 +1,9 @@
 class RecalculateCostToDresses < ActiveRecord::Migration
   def up
+    dresses = Dress.where('created_at > "2014-04-29"')
+    dresses.each do |dress|
+      dress.update_attribute :net_cost, dress.price - (dress.price.to_f * dress.supplier_account.net_margin.to_f*1.19/100).ceil
+    end
   end
 
   def down
